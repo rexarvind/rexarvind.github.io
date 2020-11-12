0da7e95e06123bf40ac3b5d0333491ba405f7436
Arvind Kumar <thatisarvind@gmail.com>
2020-11-12 13:05:09 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -4,28 +4,6 @@ function numberWithCommas(x){return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g
 
 const projects=_("projects")
 
-fetch("https://rexarvind.github.io/lib/projects.json").then(res=>res.json())
-.then(res=>{
-res.forEach(data=>{
-/*
-projects.innerHTML+=`
-<div class="col-sm-6 col-md-4">
-<div class="card border-secondary shadow">
-<img src="img/dummy/hd720.svg" data-src="projects/${data.img}" class="lazyload card-img-top" alt="${data.text}" onclick="imgOpen(this)">
-<div class="card-footer p-1 border-secondary">
-<a href="${data.link}" target="_blank" rel="noopener noreferrer" class="btn btn-dark btn-block btn-sm shadow-none font-weight-bold text-truncate bg-gradient">${data.text}&#8599;</a></div>
-
-</div>
-</div>
-`
-
-*/
-})
-
-
-})
-.catch(err=>projects.innerHTML=err)
-
 const imgOpen=el=>{
 let modal=new bootstrap.Modal(_("rexModal"));
 _("rexModalBody").innerHTML=`
